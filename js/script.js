ff07860c7bb99547b21d483ba79339791a694353
Pavel Naumovich <pavelnaumovic@gmail.com>
2023-01-30 21:49:13 +0000

    fivedayForecastMarkup added

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ const API_KEY = "9898e02d6af866ce7ab5c03326fcf48d";
 async function getCoordinates(cityName) {
   const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
   const response = await fetch(url);
-  const data = await response.json();
-  return data;
+  return await response.json();
 }
 
 async function getTodaysWeather(cityName) {
   if (cityName.length === 0) return;
   return getCoordinates(cityName).then(async (coords) => {
-    if (coords.length) {
+    if (coords) {
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0].lat}&lon=${coords[0].lon}&appid=${API_KEY}&units=metric`;
       const response = await fetch(url);
       return await response.json();
@@ -37,7 +36,7 @@ async function getFiveDaysForecast(cityName) {
   });
 }
 
-function createDayWeatherMarkup(dayWeatherData) {
+function weatherNowMarkup(dayWeatherData) {
   const year = dayWeatherData.dt_txt.substring(0, 4);
   const month = dayWeatherData.dt_txt.substring(5, 7);
   const day = dayWeatherData.dt_txt.substring(8, 10);
@@ -47,10 +46,28 @@ function createDayWeatherMarkup(dayWeatherData) {
 
   const markup = `<div class="day-weather-card">
     <li><h3>${day}/${month}/${year}</h3></li>
-    <li>${dayWeatherData.main.temp}</li>
-    <li>${dayWeatherData.wind.speed}</li>
-    <li>${dayWeatherData.main.humidity}</li>
+    <li>${temperature}</li>
+    <li>${windSpeed}</li>
+    <li>${humidity}</li>
   </div>`;
+
+  return markup;
+}
+
+function fiveDaysForecastMarkup(weatherData) {
+  let markup = "";
+
+  for (dayData of weatherData) {
+    markup += `
+    <div class="day-weather-card">
+      <li><h3>London</h3></li>
+      <li>img</li>
+      <li>Temp${dayData.main.temp}C\` </li>
+      <li>Wind: ${dayData.wind.speed} KM/H</li>
+      <li>Humidity: ${dayData.main.humidity} %</li>
+    </div>
+    `;
+  }
 
   return markup;
 }
@@ -59,6 +76,6 @@ function renderFiveDaysForecast(forecastData) {
   const fiveDaysForecast = $("#five-day-forecast");
 }
 
-getFiveDaysForecast("london").then((results) => {
-  createDayWeatherMarkup(results[0]);
-});
+getFiveDaysForecast("london").then((data) =>
+  console.log(fiveDaysForecastMarkup(data))
+);
