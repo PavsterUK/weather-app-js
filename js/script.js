4605319a8f7c097d0fefaaac1ebdc1d05d6fd960
Pavel Naumovich <pavelnaumovic@gmail.com>
2023-01-31 21:12:27 +0000

    render one day and five day forecast

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const API_KEY = "9898e02d6af866ce7ab5c03326fcf48d";
+const fiveDaysForecast = $("#five-day-forecast");
+const weatherNow = $(".weather-now");
 
-async function getCoordinates(cityName) {
-  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+async function getPlaceCoords(placeName) {
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${placeName}&limit=1&appid=${API_KEY}`;
   const response = await fetch(url);
   return await response.json();
 }
 
-async function getTodaysWeather(cityName) {
-  if (cityName.length === 0) return;
-  return getCoordinates(cityName).then(async (coords) => {
+async function getTodayWeather(placeName) {
+  if (placeName.length === 0) return;
+  return getPlaceCoords(placeName).then(async (coords) => {
     if (coords) {
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0].lat}&lon=${coords[0].lon}&appid=${API_KEY}&units=metric`;
       const response = await fetch(url);
@@ -17,10 +19,10 @@ async function getTodaysWeather(cityName) {
   });
 }
 
-async function getFiveDaysForecast(cityName) {
-  if (cityName.length === 0) return;
-  return getCoordinates(cityName).then(async (coords) => {
-    if (coords.length) {
+async function getFiveDaysForecast(placeName) {
+  if (placeName.length === 0) return;
+  return getPlaceCoords(placeName).then(async (coords) => {
+    if (coords) {
       const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0].lat}&lon=${coords[0].lon}&appid=${API_KEY}&units=metric`;
       const response = await fetch(url);
       const data = await response.json();
@@ -30,52 +32,51 @@ async function getFiveDaysForecast(cityName) {
       for (let i = 0; i < data.list.length; i += 8) {
         forecasts.push(data.list[i]);
       }
-
       return forecasts;
     }
   });
 }
 
-function weatherNowMarkup(dayWeatherData) {
-  const year = dayWeatherData.dt_txt.substring(0, 4);
-  const month = dayWeatherData.dt_txt.substring(5, 7);
-  const day = dayWeatherData.dt_txt.substring(8, 10);
-  const temperature = `Temp: ${dayWeatherData.main.temp} C`;
-  const windSpeed = `Wind: ${dayWeatherData.wind.speed} KM/H`;
-  const humidity = `Humidity: ${dayWeatherData.main.humidity} %`;
-
-  const markup = `<div class="day-weather-card">
-    <li><h3>${day}/${month}/${year}</h3></li>
-    <li>${temperature}</li>
-    <li>${windSpeed}</li>
-    <li>${humidity}</li>
+function getDayWeatherMarkup(weatherData, placeInfoMarkup = "") {
+  const markup = `
+  <div class="day-weather-card">
+    ${placeInfoMarkup}
+    <li>Temp: ${dayWeatherData.main.temp} C</li>
+    <li>Wind: ${dayWeatherData.wind.speed} KM/H</li>
+    <li>Humidity: ${dayWeatherData.main.humidity} %</li>
   </div>`;
-
   return markup;
 }
 
-function fiveDaysForecastMarkup(weatherData) {
+function renderTodayWeather(weatherData) {
+  const year = weatherData.dt_txt.substring(0, 4);
+  const month = weatherData.dt_txt.substring(5, 7);
+  const day = weatherData.dt_txt.substring(8, 10);
+  const placeInfoMarkup = `<li><h3>${day}/${month}/${year}</h3></li>`;
+
+  weatherNow.append(getDayWeatherMarkup(weatherData, placeInfoMarkup));
+}
+
+function getFiveDaysForecastMarkup(weatherData) {
   let markup = "";
 
   for (dayData of weatherData) {
-    markup += `
-    <div class="day-weather-card">
-      <li><h3>London</h3></li>
-      <li>img</li>
-      <li>Temp${dayData.main.temp}C\` </li>
-      <li>Wind: ${dayData.wind.speed} KM/H</li>
-      <li>Humidity: ${dayData.main.humidity} %</li>
-    </div>
-    `;
+    markup += getDayWeatherMarkup(dayData);
   }
 
   return markup;
 }
 
-function renderFiveDaysForecast(forecastData) {
-  const fiveDaysForecast = $("#five-day-forecast");
-}
+function onSearchButtonClickEventHandler(event) {
+  const placeName = event.target.value;
+  getPlaceCoords(placeName).then((coords) => {
+    getTodayWeather(coords).then((todayWeatherData) => {
+      renderTodayWeather(todayWeatherData);
+    });
 
-getFiveDaysForecast("london").then((data) =>
-  console.log(fiveDaysForecastMarkup(data))
-);
+    getFiveDaysForecast(coords).then(fiveDayWeatherData => {
+
+    })
+
+  });
+}
