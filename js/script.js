82d63a11285e9706c3825594baa994d0d94c5c27
Pavel Naumovich <pavelnaumovic@gmail.com>
2023-02-01 21:50:07 +0000

    render weather data on load, and on entering location, save search history to local storage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const API_KEY = "9898e02d6af866ce7ab5c03326fcf48d";
 const fiveDaysForecast = $("#five-day-forecast");
 const weatherNow = $(".weather-now");
+const searchHistory = $("#history");
 
 async function getPlaceCoords(placeName) {
   const url = `https://api.openweathermap.org/geo/1.0/direct?q=${placeName}&limit=1&appid=${API_KEY}`;
@@ -8,53 +9,54 @@ async function getPlaceCoords(placeName) {
   return await response.json();
 }
 
-async function getTodayWeather(placeName) {
-  if (placeName.length === 0) return;
-  return getPlaceCoords(placeName).then(async (coords) => {
-    if (coords) {
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0].lat}&lon=${coords[0].lon}&appid=${API_KEY}&units=metric`;
-      const response = await fetch(url);
-      return await response.json();
-    }
-  });
+async function getTodayWeather(coords) {
+  if (coords) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0].lat}&lon=${coords[0].lon}&appid=${API_KEY}&units=metric`;
+    const response = await fetch(url);
+    return await response.json();
+  }
 }
 
-async function getFiveDaysForecast(placeName) {
-  if (placeName.length === 0) return;
-  return getPlaceCoords(placeName).then(async (coords) => {
-    if (coords) {
-      const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0].lat}&lon=${coords[0].lon}&appid=${API_KEY}&units=metric`;
-      const response = await fetch(url);
-      const data = await response.json();
-      let forecasts = [];
+async function getFiveDaysForecast(coords) {
+  if (coords) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${coords[0].lat}&lon=${coords[0].lon}&appid=${API_KEY}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    let forecasts = [];
 
-      //Only show forecast for one hour every 24 hours from current time.
-      for (let i = 0; i < data.list.length; i += 8) {
-        forecasts.push(data.list[i]);
-      }
-      return forecasts;
+    //Only show forecast for one hour every 24 hours from request time.
+    for (let i = 0; i < data.list.length; i += 8) {
+      forecasts.push(data.list[i]);
     }
-  });
+    return forecasts;
+  }
 }
 
 function getDayWeatherMarkup(weatherData, placeInfoMarkup = "") {
   const markup = `
   <div class="day-weather-card">
     ${placeInfoMarkup}
-    <li>Temp: ${dayWeatherData.main.temp} C</li>
-    <li>Wind: ${dayWeatherData.wind.speed} KM/H</li>
-    <li>Humidity: ${dayWeatherData.main.humidity} %</li>
+    <li>Temp: ${weatherData.main.temp} C</li>
+    <li>Wind: ${weatherData.wind.speed} KM/H</li>
+    <li>Humidity: ${weatherData.main.humidity} %</li>
   </div>`;
   return markup;
 }
 
-function renderTodayWeather(weatherData) {
-  const year = weatherData.dt_txt.substring(0, 4);
-  const month = weatherData.dt_txt.substring(5, 7);
-  const day = weatherData.dt_txt.substring(8, 10);
-  const placeInfoMarkup = `<li><h3>${day}/${month}/${year}</h3></li>`;
+const _unixToDate = (unixStamp) => {
+  return moment.unix(unixStamp).format("dddd, Do MMM YYYY");
+};
 
-  weatherNow.append(getDayWeatherMarkup(weatherData, placeInfoMarkup));
+function renderTodayWeather(weatherData) {
+  const placeName = weatherData.name;
+  const dateNow = _unixToDate(weatherData.dt);
+  const placeInfoMarkup = `<li><h3>${placeName} (${dateNow})</h3></li>`;
+
+  weatherNow.html(getDayWeatherMarkup(weatherData, placeInfoMarkup));
+}
+
+function renderFiveDaysForecast(weatherData) {
+  fiveDaysForecast.html(getFiveDaysForecastMarkup(weatherData));
 }
 
 function getFiveDaysForecastMarkup(weatherData) {
@@ -67,16 +69,36 @@ function getFiveDaysForecastMarkup(weatherData) {
   return markup;
 }
 
-function onSearchButtonClickEventHandler(event) {
-  const placeName = event.target.value;
+function onSearchButtonClickEventHandler() {
+  const placeName = $("#user-input").val();
+  init(placeName);
+}
+
+function init(placeName) {
+  syncLocalStorage(placeName);
   getPlaceCoords(placeName).then((coords) => {
     getTodayWeather(coords).then((todayWeatherData) => {
       renderTodayWeather(todayWeatherData);
     });
 
-    getFiveDaysForecast(coords).then(fiveDayWeatherData => {
-
-    })
-
+    getFiveDaysForecast(coords).then((fiveDayWeatherData) => {
+      renderFiveDaysForecast(fiveDayWeatherData);
+    });
   });
 }
+
+function syncLocalStorage(placeName) {
+  if (!localStorage.getItem("placeName")) {
+    localStorage.setItem("placeName", JSON.stringify([]));
+  }
+  const places = JSON.parse(localStorage.getItem("placeName"));
+  const placeButtonMarkup = `
+    <button onclick="onSearchButtonClickEventHandler(event)">
+      ${placeName}
+    </button>`;
+  places.push(placeButtonMarkup);
+  localStorage.setItem("placeName", JSON.stringify(places));
+  searchHistory.html(places);
+}
+
+init("London");
